b8bb0a38b631e8c1031a26852f7a063e7e850c50
hoangquocvuonghp8989-alt <hoangquocvuong.hp8989@gmail.com>
2026-04-18 10:13:05 +0700

    Create base-map.js

---
+++ base-map.js
@@ -0,0 +1,7 @@
+const BASE_MAP = {
+  "529391": "https://link.clashofclans.com/en?action=OpenLayout&id=TH14%3AWB%3AAAAAKwAAAAKRj0tgC3gvg4wQm7CNnDN5",
+
+  "529392": "https://link.clashofclans.com/en?action=OpenLayout&id=TH14%3AWB%3AAAAA....",
+
+  "529393": "https://link.clashofclans.com/en?action=OpenLayout&id=TH15%3AWB%3AAAAA...."
+};
